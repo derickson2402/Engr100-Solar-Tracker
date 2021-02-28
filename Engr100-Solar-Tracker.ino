61c9ca001db156a697802fb35660392ad6e98735
Dan Erickson <daniel@dans-skynet.com>
2021-02-28 18:06:01 -0500

    Create cardinal coordinate system, outline logic for moving servos
    Implemented a cardinal coordinate system for referencing the sensors and servos, with north and east being positive. Check the header comment in Engr100-Solar-Tracker.ino for more info.
    
    Wrote comments in loop() outlining the servo position update system. The code that updates the servo position needs to be implemented.

--- Engr100-Solar-Tracker.ino
+++ Engr100-Solar-Tracker.ino
@@ -1,7 +1,7 @@
 /*##############################################################################
 
 Date
-    February 23, 2021
+    February 28, 2021
 
 Written By
     Daniel Erickson   (danerick)
@@ -13,116 +13,87 @@ Course
     Engr 100-435
 
 Libraries
-    None (this section is a placeholder)
+    [This section is a placeholder and should be updated]
     
 Description
     This program drives two servo motors connected to an Arduino, in order to
     angle a solar panel towards a moving light source.
 
+    Sensors and rotational axes are aligned according to a cardinal directional system, with North and East being positive as such:
+
+                           N                (+)
+                         W-+-E    -->    (-)-+-(+)
+                           S                (-)
+
 
 
 ##############################################################################*/
 
+
 // Establish sensor pins, servo pins, and threshold values
-const int sensorWest = 0;           // The pin number for west photoresistor
-const int sensorEast = 1;           // The pin number for east photoresistor
-const int sensorNorth = 2;          // The pin number for north photoresistor
-const int sensorSouth = 3;          // The pin number for south photoresistor
-const int servoAnglePin = 4;        // The pin number for angle servo
-const int servoRotatePin = 5;       // The pin number for rotation servo
+const int sensorWestPin = 0;        // The pin number for west photoresistor
+const int sensorEastPin = 1;        // The pin number for east photoresistor
+const int sensorNorthPin = 2;       // The pin number for north photoresistor
+const int sensorSouthPin = 3;       // The pin number for south photoresistor
+const int servoNSPin = 4;           // The pin number for angle servo
+const int servoEWPin = 5;           // The pin number for rotation servo
 const int lightErrorThreshold = 10; // The threshold for the light level error,
+Servo servo
 //                                     below which the servo position will not
 //                                     change
 
+
 /* TODO: Currently the program uses raw light level error to determine
 actions, but it might be useful to have a conversion function so
-that actual irradiance values can be used */
+that actual irradiance values can be used
 int Convert the raw input to a 10 bit scale ( raw) {
   
 }
+*/
+
 
 void setup() {
+
   Serial.begin(9600);               // Open a serial port for remote instrument reading
-  pinMode(servoAnglePin, OUTPUT);   // Open angle servo output
-  pinMode(servoRotatePin, OUTPUT);  // Open rotation servo output
-  )
+  pinMode(servoNWPin, OUTPUT);   // Open North West servo output
+  pinMode(servoEWPin, OUTPUT);  // Open East West servo output
 
 }
+
 void loop() {
 
-  // collect raw input from sensors
+  // Calculate the error on each axis, which is the difference between the
+  // light levels on one axis
+  int lightErrorNS = analogRead(sensorSouthPin) - analogRead(sensorNorthPin);
+  int lightErrorEW = analogRead(sensorWestPin) - analogRead(sensorEastPin);
 
-  // run conversion function to get standardized light level ???
 
-  // logic to compare Left and Right sensors
-  if (right side is brighter by more than threshold) {
-    move to the right
-  } else if (left side is brighter by more than threshold) {
-    move to the left
+  // Possible TODO: run conversion function to get standardized light level ???
+
+
+  // Compare error in East West direction
+  if ( abs(lightErrorEW) > lightErrorThreshold ) {
+
+    if ( lightErrorEW > 0 ) {
+      // Move the servo to the East (positive direction)
+    }
+    else if ( lightErrorEW < 0 ) {
+      // Move the servo to the West (negative direction)
+    }
+
   }
 
-  // logic to compare rotation
-  
-}
 
+  // Compare error in North South direction
+  if ( abs(lightErrorNS) > lightErrorThreshold ) {
 
+    if ( lightErrorNS > 0 ) {
+      // Move the servo to the North (positive direction)
+    }
+    else if ( lightErrorNS < 0 ) {
+      // Move the servo to the South (negative direction)
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-void setup()
-{
-  pinMode(ledPin, OUTPUT);    // Set up the LED pin to be an output.
-  Serial.begin(9600);
-}
-
-void loop()
-{
-  lightLevel = analogRead(sensorPin);  // reads the voltage on the sensorPin
-  Serial.print(lightLevel);
-  //autoRange();  // autoRanges the min / max values you see in your room.
-
-  calibratedlightLevel = map(lightLevel, 0, 1023, 0, 255);  // scale the lightLevel from 0 - 1023 range to 0 - 255 range.
-                                                  // the map() function applies a linear scale / offset.
-                                                  // map(inputValue, fromMin, fromMax, toMin, toMax);
-  Serial.print("\t"); 		  // tab character
-  Serial.print(calibratedlightLevel);   // println prints an CRLF at the end (creates a new line after)
-
-  analogWrite(ledPin, calibratedlightLevel);    // set the led level based on the input lightLevel.
-}
-/******************************************************************
- * void autoRange()
- * 
- * This function sets a minThreshold and maxThreshold value for the
- * light levels in your setting. Move your hand / light source / etc
- * so that your light sensor sees a full range of values. This will
- * "autoCalibrate" to your range of input values.
-/*****************************************************************/
-
-void autoRange()
-{
-  if (lightLevel < minThreshold)  // minThreshold was initialized to 1023 -- so, if it's less, reset the threshold level.
-    minThreshold = lightLevel;
-
-  if (lightLevel > maxThreshold)  // maxThreshold was initialized to 0 -- so, if it's bigger, reset the threshold level.
-    maxThreshold = lightLevel;
-
-  // Once we have the highest and lowest values, we can stick them
-  // directly into the map() function.
-  // 
-  // This function must run a few times to get a good range of bright and dark values in order to work.
-
-  lightLevel = map(lightLevel, minThreshold, maxThreshold, 0, 255);
-  lightLevel = constrain(lightLevel, 0, 255);
+  }
+ 
 }
