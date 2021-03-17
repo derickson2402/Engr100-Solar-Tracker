b8501f390bd89f7fc494aa183de739a65da76af2
Dan Erickson <daniel@dans-skynet.com>
2021-03-16 22:34:20 -0400

    Update header comment

--- Engr100-Solar-Tracker.ino
+++ Engr100-Solar-Tracker.ino
@@ -16,20 +16,32 @@ Course
     Engr 100-435
 
 Description
-    This program powers an Arduino to angle a solar panel towards a moving light source. The system uses 4 photoresistors and 2 servo motors, and can output diagnostic and periodic usage data over USB or Bluetooth serial.
+    This program powers an Arduino to angle a solar panel towards a moving
+    light source. The system uses 4 photoresistors and 2 servo motors, and can
+    output diagnostic and periodic usage data over USB or Bluetooth serial.
 
     Axis Alignment
-    Sensors and rotational axes are configured with a cardinal directioin system. North/South corresponds to up/down, which changes the angle between the ground and the panel. East/West corresponds to right/left, which rotates the whole apparatus about the z axis parallel to the ground. The cardinal directions are aligned as such:
+    Sensors and rotational axes are configured with a cardinal directioin
+    system. North/South corresponds to up/down, which changes the angle between
+    the ground and the panel. East/West corresponds to right/left, which
+    rotates the whole apparatus about the z axis parallel to the ground. The
+    cardinal directions are aligned as such:
 
                            N                (+)
                          W-+-E    -->    (-)-+-(+)
                            S                (-)
 
     Movement Algorithm
-    The servo position is updated using a simple error correction algorithm. The raw input from two opposite photoresistors is compared, and every cycle the servo motor on that axis will move one tick in the brighter direction.
+    The servo position is updated using a closed loop control structure. The
+    raw inputs from both photoresistors on an axis are compared, and if the
+    percent difference is higher than the threshold the servo motor will move
+    one tick in the brighter direction.
 
     Bluetooth Serial Output
-    By default the program will send debugging and serial outputs to the default port, which goes to pin 13 and to the USB port. When bluetooth is on, this data will also be sent to the specified bluetooth port. This code is tested to work on a HC-05 Serial Bluetooth Module v1.3.
+    By default the program will send debugging and serial outputs to the
+    default port, which goes to pin 13 and to the USB port. When bluetooth is
+    on, this data will also be sent to the specified bluetooth port. This code
+    is tested to work on a HC-05 Serial Bluetooth Module v1.3.
 
 ##############################################################################*/
 
